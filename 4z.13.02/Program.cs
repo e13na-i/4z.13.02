f98be4fece6a6e68caf7dc9ae065e9b60d14b15b
yodatime <electriclikethor@gmail.com>
2025-02-13 18:30:35 +0200

    Add project files.

---
+++ 4z.13.02/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _4z._13._02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static int Power(int a, int b)
+            {
+                if (b == 0)
+                    return 1;
+                return a * Power(a, b - 1);
+            }
+
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{A}^{B} = {Power(A, B)}");
+        }
+    }
+}
