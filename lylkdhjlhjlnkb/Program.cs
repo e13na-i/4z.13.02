9b63e82791111c2b6d0017b6900788b501d3c213
yodatime <electriclikethor@gmail.com>
2025-02-13 18:53:52 +0200

    idfk

---
+++ lylkdhjlhjlnkb/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace lylkdhjlhjlnkb
+{
+    internal class Program
+    {
+        static bool isEven(int n)
+        {
+            if (n %2==0) 
+                return true;
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("number= ");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine(isEven(num));
+        }
+
+    }
+}
